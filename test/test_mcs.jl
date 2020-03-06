f3dcf38b5e36bac18f737722dfbb77fdaa58d8df
Cora <ckingdon@berkeley.edu>
2020-03-06 13:03:27 -0500

    fix test

--- test/test_mcs.jl
+++ test/test_mcs.jl
@@ -5,6 +5,8 @@ using Test
 @testset "MCS" begin
 
 m = MimiDICE2016.get_model()
+update_param!(m, :cap_damages, true)
+update_param!(m, :cap_abatecost, true)
 mcs = MimiDICE2016.get_mcs()
 sim_inst = run(mcs, m, 1000)
 
